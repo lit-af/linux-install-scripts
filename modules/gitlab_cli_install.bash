686cbe7fd3c307247240f9a1b02ec88aa02c2e74
Alexandre Francoeur <afrancoeur@uzinakod.com>
2024-04-22 17:08:54 -0400

    update gitlab version

--- modules/gitlab_cli_install.bash
+++ modules/gitlab_cli_install.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LATEST_RELEASE="1.33.0"
+LATEST_RELEASE="1.39.0"
 
 wget -q https://gitlab.com/gitlab-org/cli/-/releases/v$LATEST_RELEASE/downloads/glab_$LATEST_RELEASE\_Linux_x86_64.deb
 sudo apt install -y ./glab_$LATEST_RELEASE\_Linux_x86_64.deb --fix-broken
