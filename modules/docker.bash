792b35ebbf10c2822b8121b82e560b4f3eba1d80
Alexandre Francoeur <afrancoeur@uzinakod.com>
2024-04-23 12:08:38 -0400

    improve docker script

--- modules/docker.bash
+++ modules/docker.bash
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+# deleting rootless context if it exist
+docker context rm -f rootless
+
+# Delete docker group
+sudo groupdel docker
+
 # uninstall all conflicting packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-
 rm get-docker.sh
 
 # Post Install Steps
